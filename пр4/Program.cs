4b6347a1d0c9611303683d23ecd22f3c2c8cdd82
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-11-24 21:15:29 +0200

    Add files via upload

---
+++ пр4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1
+            //Console.WriteLine("Введите число:");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num1 > 0 ? num1 + 1 : num1 - 2);
+
+            // Задача 2
+            //Console.WriteLine("Введите число:");
+            //int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num2 > 0 ? num2 + 1 : num2);
+
+            // Задача 3
+            //Console.WriteLine("Введите число:");
+            //int num3 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num3 > 0 ? num3 + 1 : num3 < 0 ? num3 - 2 : 10);
+
+            // Задача 4
+            //Console.WriteLine("Введите число:");
+            //int num4 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num4 % 2 == 0 ? "Четное" : "Нечетное");
+
+            // Задача 5
+            //Console.WriteLine("Введите число:");
+            //int num5 = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num5 > 0 ? "Положительное" : num5 < 0 ? "Отрицательное" : "Ноль");
+
+            // Задача 6
+            //Console.WriteLine("Введите цену ноутбука:");
+            //int price = int.Parse(Console.ReadLine());
+            //Console.WriteLine(price >= 20 && price <= 30 ? "Входит в диапазон" : "Не входит в диапазон");
+
+            // Задача 7
+            //Console.WriteLine("Введите год:");
+            //int year = int.Parse(Console.ReadLine());
+            //Console.WriteLine((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? "Високосный" : "Не високосный");
+            //Console.Read();
+        }
+    }
+}
